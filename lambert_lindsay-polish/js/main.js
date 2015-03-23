e954308b614304d65f15cfe76dfefbdf3757d5f0
Lindsay Lambert <LindsayLambo@fullsail.edu>
2015-03-22 23:40:03 -0400

    Submitting, even though unfinished...

--- lambert_lindsay-polish/js/main.js
+++ lambert_lindsay-polish/js/main.js
@@ -1,3 +1,5 @@
+(function($){
+
 /*  Sortable Projects: Projects  ----------------------------------------- */	
  
   
@@ -10,3 +12,20 @@ $( "#sortable" ).disableSelection();
   
 							
 $( ".selectable" ).selectable();
+
+
+/*  Tooltips : Favorites Page  ----------------------------------------- */	
+
+
+$('.masterTooltip').hover(function() {
+		var title = $(this).attr('title');
+		$(this).data('tipText', title).removeAttr('title');
+		$('<p class="tooltip"></p>').text(title).appendTo('body').fadeIn('slow');
+	},  function() {
+		$(this).attr('title', $(this).data('tipText'));
+		$('.tooltip').remove();
+	}).mousemove(function(e) {
+		var mousex = e.pageX + 20;
+		var mousey = e.pageY + 10;
+		$('.tooltip').css({ top: mousey, left: mousex })
+});
